55e83a945cfc9520e2b410aec3e43696ee45cee5
nivedpk21 <162270753+nivedpk21@users.noreply.github.com>
2024-09-06 14:41:27 +0530

    implemented checkip

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const bodyparser = require("body-parser");
 const cors = require("cors");
 const ipModel = require("./src/models/ipModel");
 const urlModel = require("./src/models/urlModel");
+const proxyCheck = require("proxycheck-node.js");
 
 app.use(bodyparser.json());
 app.use(cors());
@@ -12,16 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/live", (req, res) => {
   res.send("hello world!");
 });
-//nice
-app.set("trust proxy", true);
 
 app.get("/get-ip", async (req, res) => {
+  const requestIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const userIp = requestIP ? requestIP.split(",")[0].trim() : req.connection.remoteAddress;
+  console.log(requestIP, "req IP");
+  console.log(userIp, "userIP");
+  testIP = "157.46.1.250";
+  console.log(testIP, "tst");
+
   try {
-    const requestIP = req.headers["x-forwarded-for"];
-    const userIp = requestIP ? requestIP.split(",")[0].trim() : req.connection.remoteAddress;
+    const check = new proxyCheck({ api_key: "989e88-762533-6f2h93-ny9541" });
+    const result = await check.check(userIp);
+    console.log(result, "result");
 
     let existingIp = await ipModel.findOne({ ip: userIp });
-
     if (!existingIp) {
       console.log("freship");
       // fresh ip
@@ -33,7 +39,10 @@ app.get("/get-ip", async (req, res) => {
       if (saveData) {
         const responseUrl = await urlModel.findOne({ order: "1" });
         if (responseUrl) {
-          return res.redirect(responseUrl.url);
+          // return res.redirect(responseUrl.url);
+          return res.status(200).json({
+            data: result,
+          });
         } else {
           return res.status(400).json({ message: "unable fetch url" });
         }
